9e2c923752807e77e105af338c8f00372956e3c1
cpon00 <cpon712@gmail.com>
2019-02-02 22:26:36 -0800

    updated Die.java.

--- homework02/Die.java
+++ homework02/Die.java
@@ -1,15 +1,25 @@
+import java.util.Random;
 public class Die {
+    sidesCount = (nSides);
+    private int sidesCount;
+    private Random randomroll;
     public Die(int nSides) {
 //Constructor for a single die with sides numbered 1, 2... up to nSides.
+    sidesCount = (nSides);
     }
     public int roll() {
-//returns a alue as a result of randomly rolling this die.
+//returns a value as a result of randomly rolling this die.
+    randomroll = new Random(nSides);
+    return (randomroll);
+
+
     }
     public int getValue() {
 //returns the current value of this die which resulted from the last roll.
     }
     public int setSides(int nSides) {
 //change the configuration of this die and return the new number of sides.
+    String setSides = console.readLine("Enter new number of sides:");
     }
     public String toString() {
 //Instance method that returns a string-y representation of THIS die, e.g., "11".
