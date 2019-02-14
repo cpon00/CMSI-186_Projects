0a8a7684bd684853e32c31db369a99ad455a5286
Carter Pon <46779317+cpon00@users.noreply.github.com>
2019-02-14 01:39:35 -0800

    created Die.java, DiceSet.java, HighRoll.java.

--- homework02/DiceSet.java
+++ homework02/DiceSet.java
@@ -1,113 +1,141 @@
 /** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- *  File name     :  DiceSet.java
- *  Purpose       :  Provides a class describing a set of dice
- *  Author        :  B.J. Johnson
- *  Date          :  2017-02-09
- *  Description   :  This class provides everything needed (pretty much) to describe a set of dice.  The
- *                   idea here is to have an implementing class that uses the Die.java class.  Includes
- *                   the following:
- *                   public DiceSet( int k, int n );                  // Constructor for a set of k dice each with n-sides
- *                   public int sum();                                // Returns the present sum of this set of dice
- *                   public void roll();                              // Randomly rolls all of the dice in this set
- *                   public void rollIndividual( int i );             // Randomly rolls only the ith die in this set
- *                   public int getIndividual( int i );               // Gets the value of the ith die in this set
- *                   public String toString();                        // Returns a stringy representation of this set of dice
- *                   public static String toString( DiceSet ds );     // Classwide version of the preceding instance method
- *                   public boolean isIdentical( DiceSet ds );        // Returns true iff this set is identical to the set ds
- *                   public static void main( String[] args );        // The built-in test program for this class
- *
- *  Notes         :  Stolen from Dr. Dorin pretty much verbatim, then modified to show some interesting
- *                   things about Java, and to add this header block and some JavaDoc comments.
- *  Warnings      :  None
- *  Exceptions    :  IllegalArgumentException when the number of sides or pips is out of range
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- *  Revision Histor
- *  ---------------
- *            Rev      Date     Modified by:  Reason for change/modification
- *           -----  ----------  ------------  -----------------------------------------------------------
- *  @version 1.0.0  2017-02-09  B.J. Johnson  Initial writing and release
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+*  File name     :  DiceSet.java
+*  Purpose       :  Provides a class describing a set of dice
+*  Author        :  B.J. Johnson
+*  Date          :  2017-02-09
+*  Description   :  This class provides everything needed (pretty much) to describe a set of dice.  The
+*                   idea here is to have an implementing class that uses the Die.java class.  Includes
+*                   the following:
+*                   public DiceSet( int k, int n );                  // Constructor for a set of k dice each with n-sides
+*                   public int sum();                                // Returns the present sum of this set of dice
+*                   public void roll();                              // Randomly rolls all of the dice in this set
+*                   public void rollIndividual( int i );             // Randomly rolls only the ith die in this set
+*                   public int getIndividual( int i );               // Gets the value of the ith die in this set
+*                   public String toString();                        // Returns a stringy representation of this set of dice
+*                   public static String toString( DiceSet ds );     // Classwide version of the preceding instance method
+*                   public boolean isIdentical( DiceSet ds );        // Returns true iff this set is identical to the set ds
+*                   public static void main( String[] args );        // The built-in test program for this class
+*
+*  Notes         :  Stolen from Dr. Dorin pretty much verbatim, then modified to show some interesting
+*                   things about Java, and to add this header block and some JavaDoc comments.
+*  Warnings      :  None
+*  Exceptions    :  IllegalArgumentException when the number of sides or pips is out of range
+* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*  Revision Histor
+*  ---------------
+*            Rev      Date     Modified by:  Reason for change/modification
+*           -----  ----------  ------------  -----------------------------------------------------------
+*  @version 1.0.0  2017-02-09  B.J. Johnson  Initial writing and release
+* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+import java.util.Random;
 public class DiceSet {
-
-  /**
-   * private instance data
-   */
-   private int count;
-   private int sides;
-   private Die[] ds = null;
-
-   // public constructor:
-  /**
-   * constructor
-   * @param  count int value containing total dice count
-   * @param  sides int value containing the number of pips on each die
-   * @throws IllegalArgumentException if one or both arguments don't make sense
-   * @note   parameters are checked for validity; invalid values throw "IllegalArgumentException"
-   */
-   public DiceSet( int count, int sides ) {
-      ds = new Die[ count ];
-   }
-
-  /**
-   * @return the sum of all the dice values in the set
-   */
-   public int sum() {
-      return 0;
-   }
-
-  /**
-   * Randomly rolls all of the dice in this set
-   *  NOTE: you will need to use one of the "toString()" methods to obtain
-   *  the values of the dice in the set
-   */
-   public void roll() {
-   }
-
-  /**
-   * Randomly rolls a single die of the dice in this set indexed by 'dieIndex'
-   * @param  dieIndex int of which die to roll
-   * @return the integer value of the newly rolled die
-   * @trhows IllegalArgumentException if the index is out of range
-   */
-   public int rollIndividual( int dieIndex ) {
-      return 0;
-   }
-
-  /**
-   * Gets the value of the die in this set indexed by 'dieIndex'
-   * @param  dieIndex int of which die to roll
-   * @trhows IllegalArgumentException if the index is out of range
-   */
-   public int getIndividual( int dieIndex ) {
-      return -999;
-   }
-
-  /**
-   * @return Public Instance method that returns a String representation of the DiceSet instance
-   */
-   public String toString() {
-      String result = "";
-      return result;
-   }
-
-  /**
-   * @return Class-wide version of the preceding instance method
-   */
-   public static String toString( DiceSet ds ) {
-      return "";
-   }
-
-  /**
-   * @return  tru iff this set is identical to the set passed as an argument
-   */
-   public boolean isIdentical( DiceSet ds ) {
-      return true;
-   }
-  /**
-   * A little test main to check things out
-   */
-   public static void main( String[] args ) {
-      // You do this part!
-   }
-
+    private int count;
+    private int sides;
+    private Die[] ds = null;
+    /**
+    * constructor
+    * @param  count int value containing total dice count
+    * @param  sides int value containing the number of pips on each die
+    * @throws IllegalArgumentException if one or both arguments don't make sense
+    * @note   parameters are checked for validity; invalid values throw "IllegalArgumentException"
+    */
+    public DiceSet(int count, int sides) {
+        if (count > 1 && sides > 3) {
+            ds = new Die [count];
+            for (int i = 0; i < count; i++) {
+                ds[i] = new Die(sides);
+            }
+        }else{
+            throw new IllegalArgumentException("Invalid DiceSet.");
+        }
+        this.count = count;
+        this.sides = sides;
+    }
+    /**
+    * @return the sum of all the dice values in the set
+    */
+    public int sum() {
+        int diceSum = 0;
+        for (int i = 0; i < count; i++) {
+            diceSum += ds[i].getValue();
+        }
+        return diceSum;
+    }
+    /**
+    * Randomly rolls all of the dice in this set
+    *  NOTE: you will need to use one of the "toString()" methods to obtain
+    *  the values of the dice in the set
+    */
+    public void roll() {
+        for (int i = 0; i < count; i++) {
+            ds[i].roll();
+            ds[i].toString();
+        }
+    }
+    /**
+    * Randomly rolls a single die of the dice in this set indexed by 'dieIndex'
+    * @param  dieIndex int of which die to roll
+    * @return the integer value of the newly rolled die
+    * @throws IllegalArgumentException if the index is out of range
+    */
+    public int rollIndividual( int dieIndex ) {
+        if (dieIndex < 0 || dieIndex > ds.length) {
+            throw new IllegalArgumentException("Invalid rollIndividual dieIndex.");
+        }
+        ds[dieIndex].roll();
+        return (ds[dieIndex].getValue());
+    }
+    /**
+    * Gets the value of the die in this set indexed by 'dieIndex'
+    * @param  dieIndex int of which die to roll
+    * @throws IllegalArgumentException if the index is out of range
+    */
+    public int getIndividual( int dieIndex ) {
+        if (dieIndex < 0 || dieIndex > ds.length) {
+            throw new IllegalArgumentException("Invalid getIndividual dieIndex.");
+        }
+        return (ds[dieIndex].getValue());
+    }
+    /**
+    * @return Public Instance method that returns a String representation of the DiceSet instance
+    */
+    public String toString() {
+        String result = "";
+        for (int i = 0; i < count; i++) {
+            result += ds[i].toString();
+        }
+        return result;
+    }
+    /**
+    * @return Class-wide version of the preceding instance method
+    */
+    public static String toString( DiceSet ds ) {
+        return (ds.toString());
+    }
+    /**
+    * @return  true if this set is identical to the set passed as an argument
+    */
+    public boolean isIdentical( DiceSet ds ) {
+        return (this.count == ds.count && this.sides == ds.sides);
+    }
+    /**
+    * A little test main to check things out
+    */
+    public static void main( String[] args ) {
+        DiceSet testDiceSet = new DiceSet(12,6);
+        System.out.println("testDiceSet.sides");
+        System.out.println(testDiceSet.sides);
+        System.out.println("testDiceSet.count");
+        System.out.println(testDiceSet.count);
+        System.out.println("testDiceSet.roll()");
+        testDiceSet.roll();
+        System.out.println("testDiceSet.toString()");
+        System.out.println(testDiceSet.toString());
+        System.out.println("testDiceSet.getIndividual(0)");
+        System.out.println(testDiceSet.getIndividual(0));
+        System.out.println("testDiceSet.rollIndividual(0)");
+        System.out.println(testDiceSet.rollIndividual(0));
+        System.out.println("testDiceSet.toString()");
+        System.out.println(testDiceSet.toString());
+    }
 }
---
+++ homework02/HighRoll.java
@@ -0,0 +1,97 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*  File name     :  HighRoll.java
+*  Purpose       :  Demonstrates the use of input from a command line for use with DiceSet.java and Die.java
+*  Author        :  B.J. Johnson
+*  Date          :  2017-02-14
+*  Description   :
+*  Notes         :  None
+*  Warnings      :  None
+*  Exceptions    :  None
+* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*  Revision Histor
+*  ---------------
+*            Rev      Date     Modified by:  Reason for change/modification
+*           -----  ----------  ------------  -----------------------------------------------------------
+*  @version 1.0.0  2017-02-14  B.J. Johnson  Initial writing and release
+*  @version 1.1.0  2019-02-14  Carter Pon    Creation of HighRoll method
+* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+public class HighRoll{
+    public static void main( String args[] ) {
+        String diceInput = "";
+        int numberOfDice = 0;
+        String sidesInput = "";
+        int numberOfSides = 0;
+        String rollIndividualString = "";
+        int rollIndividualInteger = 0;
+        int highScore = 0;
+        BufferedReader input = new BufferedReader( new InputStreamReader( System.in ) );
+        try {
+            System.out.print("Number of dice: ");
+            diceInput = input.readLine();
+            //diceInput is a string.
+            numberOfDice = Integer.parseInt(diceInput);
+            //numberOfDice = diceInput converted to an integer.
+            System.out.print("Number of sides on each die: ");
+            sidesInput = input.readLine();
+            //string.
+            numberOfSides = Integer.parseInt(sidesInput);
+            //numberOfDice = sidesInput converted to an integer.
+        } catch (Exception e) {
+            System.out.println("Invalid DiceSet.");
+        }
+        System.out.println("\n Welcome to High Roll!!\n" );
+        System.out.println(" Press [1] to: ROLL ALL THE DICE.");
+        System.out.println(" Press [2] to: ROLL A SINGLE DIE.");
+        System.out.println(" Press [3] to: CALCULATE THE SCORE FOR THIS SET.");
+        System.out.println(" Press [4] to: SAVE THIS SCORE AS HIGH SCORE.");
+        System.out.println(" Press [5] to: DISPLAY THE HIGH SCORE.");
+        System.out.println(" Press [q] to: QUIT THE PROGRAM.");
+        DiceSet ds = new DiceSet(numberOfDice, numberOfSides);
+        while (true) {
+            System.out.print("Enter an input: ");
+            String inputLine = null;
+            try {
+                inputLine = input.readLine();
+                switch (inputLine.charAt(0)) {
+                    case '1': ds.roll();
+                              //rolls DiceSet and returns a string of the DiceSet.
+                              System.out.println("DiceSet :" + ds.toString());
+                              break;
+                    case '2': System.out.print("Which die do you wish to reroll? (Starting at 0...): ");
+                              //creates a new user input that determines which die to reroll.
+                              //stylistically similar to the previous inputs for numberOfDice, numberOfSides.
+                              rollIndividualString = input.readLine();
+                              rollIndividualInteger = Integer.parseInt(rollIndividualString);
+                              System.out.println("Die at "+ "DiceSet["+ (rollIndividualInteger) +"] " + "changed to " + ds.rollIndividual(rollIndividualInteger) + ".");
+                              System.out.println("New DiceSet: " + ds.toString());
+                              break;
+                    case '3': System.out.println("Current score is: " + ds.sum());
+                              //adds all values of the DiceSet
+                              break;
+                    case '4': if (ds.sum() > highScore) {
+                              //if the current score is greater than the high score, change the high score.
+                                  highScore = ds.sum();
+                                  System.out.println("Wow! High Score is now: " + highScore);
+                              }else{
+                              //else try again...
+                                  System.out.println("git gud kid... Score is too low.");
+                              }
+                              break;
+                    case '5': System.out.println("Current high score is : " + highScore);
+                              //print current high score.
+                              break;
+                    case 'q': System.out.println("Ending HighRoll...");
+                              //break the while statement.
+                              return;
+                    default: System.out.println("Invalid input.");
+                             //if input is anything other than specified switch cases.
+                }
+            }catch (Exception e) {
+                System.out.println("Exception: Invalid input.");
+            }
+        }
+    }
+}
