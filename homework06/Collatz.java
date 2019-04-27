8f3686ea404ae1e367738c5b2764fbe256d20d87
cpon00 <cpon712@gmail.com>
2019-04-27 15:56:41 -0700

    final commit

--- homework06/Collatz.java
+++ homework06/Collatz.java
@@ -4,10 +4,21 @@ import java.util.List;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*  User inputs a number that is then collated.
+*  @param  args  String array which contains command line arguments
+*  NOTE:  args[0] must be positive.
+*  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
 public class Collatz{
 	public static void main (String[] args) {
 		int count = 0;
+
 		BrobInt value = new BrobInt(args[0]);
+		if (value.sign != 0) {
+			throw new UnsupportedOperationException ("Must be positive number.");
+		}
 		while (value.compareTo(BrobInt.ONE) == 1) {
 			System.out.println("Current Value:    " + value);
 			if (value.remainder(BrobInt.TWO) != BrobInt.ZERO) {
